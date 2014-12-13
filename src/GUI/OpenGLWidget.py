7270681b01f8581a440bea9c2674ebda105df97b
t00n <antoine.carpentier.info@gmail.com>
2014-12-13 22:04:48 +0100

    [add] floor grid

--- src/GUI/OpenGLWidget.py
+++ src/GUI/OpenGLWidget.py
@@ -156,6 +156,9 @@ class OpenGLWidget(QtOpenGL.QGLWidget):
         self.floor['position'] =  [[ 1, 0, 1], [-1, 0, 1], [-1, 0.1, 1], [ 1,0.1, 1],
                  [ 1,0.1,-1], [ 1, 0,-1], [-1, 0,-1], [-1,0.1,-1]]
         self.floor['scale'] = 1.0
+        I = [0,1,2, 0,2,3,  0,3,4, 0,4,5,  0,5,6, 0,6,1,
+             1,6,7, 1,7,2,  7,4,3, 7,3,2,  4,7,6, 4,6,5]
+        self.floor_indices = IndexBuffer(I)
 
     def loadObjects(self):
         """ docstring """
@@ -189,10 +192,14 @@ class OpenGLWidget(QtOpenGL.QGLWidget):
         self.floor['model'] = self.model
         self.floor['view'] = self.view
         self.floor['projection'] = self.projection
-        I = [0,1,2, 0,2,3,  0,3,4, 0,4,5,  0,5,6, 0,6,1,
-             1,6,7, 1,7,2,  7,4,3, 7,3,2,  4,7,6, 4,6,5]
-        indices = IndexBuffer(I)
-        self.floor.draw(gl.GL_TRIANGLE_STRIP, indices)
+        self.floor['color'] = (0.5,0.5,0.5,1)
+        self.floor.draw(gl.GL_TRIANGLE_STRIP, self.floor_indices)
+        O = [0,1, 1,2, 2,3, 3,0,
+             4,7, 7,6, 6,5, 5,4,
+             0,5, 1,6, 2,7, 3,4 ]
+        outline = IndexBuffer(O)
+        self.floor['color'] = (1,1,1,1)
+        self.floor.draw(gl.GL_LINES, outline)
 
     def paintObjects(self):
         """ docstring """
