864e0611d1dc214fa70fe6c3af24209077f0940c
t00n <antoine.carpentier.info@gmail.com>
2015-02-25 12:14:40 +0100

    [fix] SceneObject to be used later and fcking abstract things

--- src/GL/SceneObject.py
+++ src/GL/SceneObject.py
@@ -3,10 +3,35 @@
 
 
 class SceneObject:
-	def __init__(self, program, position, color, indices=None, outline = None, visible = True):
-		self.program = program
-		self.position = position
-		self.color = color
-		self.indices = indices
-		self.outline = outline
-		self.visible = visible
+	def __init__(self, vertices, indices, normals, position, color=None, texture=None, outline = None, visible = True):
+        self._vertices = [[vertex[i]+position[i] for i in range(len(vertex))] for vertex in vertices]
+		try:
+			self._indices = reduce(add, indices, [])
+		except:
+			self._indices = indices
+		self._normals = normals
+		self._color = color
+		self._texture = texture
+		self._outline = outline
+		self._visible = visible
+
+	def getVertices(self):
+		return self._vertices
+
+	def getIndices(self):
+		return self._indices
+
+	def getNormals(self):
+		return self._normals
+
+	def getColor(self):
+		return self._color
+
+	def getTexture(self):
+		return self._texture
+
+	def hasOutline(self):
+		return self._outline
+
+	def isVisible(self):
+		return self._visible
