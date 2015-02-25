2ea481c45fc707ad3096a7bc1b8963abc3e93cdf
t00n <antoine.carpentier.info@gmail.com>
2015-02-25 17:23:01 +0100

    [fix] bugs draw/glsl

--- src/GL/Algorithms.py
+++ src/GL/Algorithms.py
@@ -176,9 +176,7 @@ class NoShadowAlgorithm:
 
         self.active = True
 
-
         self._program['position'] = self._positions
-        self._program.draw('triangles', self._indices)
 
     def update(self):
         if self.active:
@@ -215,11 +213,8 @@ class SelfShadowAlgorithm:
 
         self.active = True
 
-        self._program['u_light_intensity'] = self._light.getIntensity()
-        self._program['u_light_position'] = self._light.getPosition()
         self._program['normal'] = self._normals
         self._program['position'] = self._positions
-        self._program.draw('triangles', self._indices)
 
     def update(self):
         if self.active:
